940b58b41f60f5a45c10e6c5a6d47e082c06a955
rakeshy19 <rakeshy19@gmail.com>
2024-02-10 16:47:00 +0530

    js inside jsx

--- App.js
+++ App.js
@@ -4,9 +4,19 @@ import ReactDOM from "react-dom/client";
 //const heading = React.createElement("h1", { "id": "heading"}, 'Namaste React');
 
 // Jsx is not html inside javascript or react . It is html or xml like syntax
+
+const title = (
+  <h1 className="head" tabIndex="5">
+    {" "}
+    Namste Recat !
+  </h1>
+);
+
 // React element
+// you can write javascript inside jsx
 const HeadingComponent = () => (
   <div id="container">
+    {title}
     <h1 className="heading">Namaste react functional component</h1>
   </div>
 );
