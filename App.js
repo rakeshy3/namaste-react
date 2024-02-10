da6a6f243ee1df44920a9a6facc2b2fc43b8223c
rakeshy19 <rakeshy19@gmail.com>
2024-02-10 16:08:14 +0530

    updated

--- App.js
+++ App.js
@@ -4,7 +4,12 @@ import ReactDOM from "react-dom/client";
 //const heading = React.createElement("h1", { "id": "heading"}, 'Namaste React');
 
 // Jsx is not html inside javascript or react . It is html or xml like syntax
-const jsxheading = <h1 id="heading">Namaste React</h1>;
+// React element
+const HeadingComponent = () => (
+  <div id="container">
+    <h1 className="heading">Namaste react functional component</h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxheading);
+root.render(<HeadingComponent />);
