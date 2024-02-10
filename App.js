2ded98012d86517c0d1cb8e46d5ecebc964c8884
rakeshy19 <rakeshy19@gmail.com>
2024-02-10 23:03:31 +0530

    test

--- App.js
+++ App.js
@@ -1,6 +1,46 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const styleCard = {
+  backgroundColor: "#f0f0f0"
+}
+
+const RestaurantCrad = () => {
+  return (
+    <div className="res-card" style={styleCard}>
+      <img className="res-card-img" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/uubzuxzz2nps1zylts6f" />
+       <h3> Meghna Foods</h3>
+       <h4> North Indian</h4>
+       <h4>4.4 star</h4>
+       <h4> 38 min</h4>
+    </div>
+  )
+}
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCrad />
+        <RestaurantCrad />
+        <RestaurantCrad />
+        <RestaurantCrad />
+        <RestaurantCrad />
+        <RestaurantCrad />
+        <RestaurantCrad />
+        <RestaurantCrad />
+        <RestaurantCrad />
+        <RestaurantCrad />
+        <RestaurantCrad />
+        <RestaurantCrad />
+        <RestaurantCrad />
+        <RestaurantCrad />
+      </div>
+    </div>
+  )
+}
+
 const Header = () => {
   return (
     <div className="header">
@@ -23,6 +63,7 @@ const AppLayout = () => {
   return (
     <div className="app">
       <Header/>
+      <Body/>
     </div>
   )
 }
