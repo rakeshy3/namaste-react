3ecfee4b4e0ea6a3fa33ce99f67edc5be7e713b2
rakeshy19 <rakeshy19@gmail.com>
2024-02-11 21:43:37 +0530

    test

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,22 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import RestaurantCrad from './RestaurantCard';
 import rseObj from '../utils/mockData';
  const Body = () => {
+
+  const fetchData = async () => {
+    const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
+
+    const json = await data.json();
+    const { data: { cards } } = json;
+    const resList = cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+    setListOfRestaurant(resList);
+  }
  
-const [listOfRestaurant, setListOfRestaurant] = useState(rseObj);
+  useEffect(() => {
+    console.log('use effects called');
+    fetchData();
+  }, [])
+  const [listOfRestaurant, setListOfRestaurant] = useState([]);
     return (
       <div className="body">
         <div className="filter">
